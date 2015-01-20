2467a6337e67176a314d63ce45c5bef01a04b135
Ruben de Vries <ruben@rubensayshi.com>
2015-01-20 11:34:02 +0100

    using batch subscribe in example

--- examples/setup_webhook/index.php
+++ examples/setup_webhook/index.php
@@ -14,14 +14,14 @@ $blocktrail = new BlocktrailSDK("MY_APIKEY", "MY_APISECRET");
 $webhookID = "my-example-webhook";
 
 // create the webhook, you should put a real URL here
-$blocktrail->setupWebhook("<URL_TO_YOUR_WEBHOOK>", $webhookID);
+$blocktrail->setupWebhook("http://rubensayshi.ngrok.com/webhook/test", $webhookID);
 
 // subscribe to receive a webhook call when a new block is found
 $blocktrail->subscribeNewBlocks($webhookID);
 
 // subscribe to receive a webhook call when a transaction happens on 1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp
 //  you will receive a webhook call for every unconfirmed transaction and the first 6 confirmations
-$blocktrail->subscribeAddressTransactions($webhookID, "1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp",6);
+$blocktrail->subscribeAddressTransactions($webhookID, "1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", 6);
 
 // if there's a `examples/setup_webhook/addresses.csv` we loop over it
 //  for every value in there that's a valid address we subscribe
@@ -30,21 +30,19 @@ if (file_exists(__DIR__ . "/addresses.csv")) {
     $lineSeperator = strpos($file, "\r\n") !== false ? "\r\n" : "\n";
     $lines = explode($lineSeperator, $file);
 
+    $addresses = [];
     foreach ($lines as $line) {
         $values = explode(",", $line);
         foreach ($values as $value) {
             // check if this value is a valid address
             if (BitcoinLib::validate_address($value)) {
-                try {
-                    $blocktrail->subscribeAddressTransactions($webhookID, $value, 6);
-                } catch (BlocktrailSDKException $e) {
-                    if (strpos($e->getMessage(), "already exists") !== false) {
-                        continue;
-                    } else {
-                        throw $e;
-                    }
-                }
+                $addresses[] = $value;
             }
         }
+    }
+
+    foreach (array_chunk($addresses, 500) as $chunk) {
+        $data = array_map(function($address) { return ['address' => $address, 'confirmations' => 6]; }, $chunk);
+        $blocktrail->batchSubscribeAddressTransactions($webhookID, $data);
     }
 }
